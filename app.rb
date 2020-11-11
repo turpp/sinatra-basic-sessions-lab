180ad1b2fdab9150a4feadff3debcf43ed4e8ab0
turpp <white.tripp@icloud.com>
2020-11-10 23:44:19 -0600

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,20 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+    configure do
+        enable :sessions
+        set :session_secret, "topsecret1"
+    end
+
+    get '/' do 
+        erb :index
+    end
+
+    post '/checkout' do
+        session['item']=params['item']
+        @session=session
+        "#{@session['item']}"
+    end
+
+
 end
